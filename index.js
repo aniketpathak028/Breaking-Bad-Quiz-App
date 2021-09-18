1265a683b42aab67854964365296c3839cdb41e5
Aniket Pathak <aniketpathak028@gmail.com>
2021-09-19 00:02:36 +0530

    logic for on-click continue button

--- index.js
+++ index.js
@@ -19,3 +19,13 @@ start_btn.onclick = ()=>{
 exit_btn.onclick = ()=>{
     info_box.classList.remove("activeInfo"); //hide info box
 }
+
+// if continueQuiz button clicked
+continue_btn.onclick = () => {
+    setTimeout(() => {
+        info_box.classList.remove("activeInfo"); //hide info box
+        quiz_box.classList.add("activeQuiz"); //show quiz box
+        showQuetions(0); //calling showQestions function
+        queCounter(1); //passing 1 parameter to queCounter
+    }, 4000)
+}
