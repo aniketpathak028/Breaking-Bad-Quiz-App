42e7b41409dc50b34a08f38d635bace126f0d982
Aniket Pathak <aniketpathak028@gmail.com>
2021-09-19 00:00:35 +0530

    selecting all elements from DOM in index.js

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 //selecting all necessary elements from DOM
+
 const start_btn = document.querySelector("#start-button");
 const info_box = document.querySelector(".info_box");
 const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
+const result_box = document.querySelector(".result_box");
+const option_list = document.querySelector(".option_list");
 
 
 // if startQuiz button clicked
+
 start_btn.onclick = ()=>{
     info_box.classList.add("activeInfo"); //show info box
 }
